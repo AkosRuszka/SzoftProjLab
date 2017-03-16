54819dd5acd3e19f87f509bd694c54769266135f
HodiSzilvi <szhodvogner@gmail.com>
2017-03-16 20:55:29 +0100

    Create KulonlegesHely
    Különleges helynek a lekódolt osztálya. (szintén teszt és még mindig buta az Eclipse)

---
+++ ModellVasut/src/vasut/KulonlegesHely.java
@@ -0,0 +1,14 @@
+package vasut;
+
+
+public class KulonlegesHely {
+	private Alagut aObserver;
+	
+	KulonlegesHely(){
+		
+	}
+	
+	public void checkTunnels(){
+		System.out.println("chechTunnel() metódus lefutott");		
+	}
+}
