366325b98bc5e4da660b4aa7cfe7d80e58bd4175
AkosRuszka <akosruszka@gmail.com>
2017-03-16 14:52:09 +0100

    Sin osztály kitöltve, kommentelve

--- ModellVasut/src/vasut/Sin.java
+++ ModellVasut/src/vasut/Sin.java
@@ -1,5 +1,33 @@
 package vasut;
 
 public class Sin {
+	private Sin aPoint;
+	private Sin bPoint;
+	private VonatElem actVonatElem;
+	private boolean dir;
 	
+	public Sin(Sin aPoint_){
+		/** Megkapja az egyik már létrehozott oldalát */
+	}
+	public Sin getAPoint() {
+		/** Lekérdezi az aPontot */
+	}
+	public Sin getBPoint() {
+		/** Lekérdezi a bPontot */
+	}
+	public void setAPoint(Sin ap) {
+		/** Beállítja az aPontot */
+	}
+	public void setBPoint(Sin bp) {
+		/** Beállítja a bPontot */
+	}
+	public void setActVonatElem(VonatElem akt) {
+		/** Beállítjuk a actVonatElem-et */
+	}
+	public VonatElem getActVonatElem() {
+		/** Lekérdezzük az actVonatElem-et */
+	}
+	public Sin actMove() {
+		/** Továbbrakjuk az actVonatElem-et */
+	}
 }
