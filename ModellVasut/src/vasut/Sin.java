fdf6acf623de6810abd36992d4ac194c4e5a03df
AkosRuszka <akosruszka@gmail.com>
2017-03-16 15:23:19 +0100

    Sintax error kiküszöbölése

--- ModellVasut/src/vasut/Sin.java
+++ ModellVasut/src/vasut/Sin.java
@@ -3,16 +3,18 @@ package vasut;
 public class Sin {
 	private Sin aPoint;
 	private Sin bPoint;
-	private VonatElem actVonatElem;
+	//private VonatElem actVonatElem;
 	private boolean dir;
 	
 	public Sin(Sin aPoint_){
 		/** Megkapja az egyik már létrehozott oldalát */
 	}
 	public Sin getAPoint() {
+		return aPoint;
 		/** Lekérdezi az aPontot */
 	}
 	public Sin getBPoint() {
+		return aPoint;
 		/** Lekérdezi a bPontot */
 	}
 	public void setAPoint(Sin ap) {
@@ -21,13 +23,13 @@ public class Sin {
 	public void setBPoint(Sin bp) {
 		/** Beállítja a bPontot */
 	}
-	public void setActVonatElem(VonatElem akt) {
+	//public void setActVonatElem(VonatElem akt) {
 		/** Beállítjuk a actVonatElem-et */
-	}
-	public VonatElem getActVonatElem() {
+	//}
+	//public VonatElem getActVonatElem() {
 		/** Lekérdezzük az actVonatElem-et */
-	}
-	public Sin actMove() {
+	//}
+	//public Sin actMove() {
 		/** Továbbrakjuk az actVonatElem-et */
-	}
+	//}
 }
