fa8439d92d3e16500c1d38d41332a4a10551b54b
AkosRuszka <akosruszka@gmail.com>
2017-03-18 20:10:23 +0100

    Osztályok "szkeletonosítása"...

--- ModellVasut/src/vasut/Allomas.java
+++ ModellVasut/src/vasut/Allomas.java
@@ -1,14 +1,13 @@
 package vasut;
 
 public class Allomas extends Sin{
-	private String color;
 	
 	public Allomas(Sin apoint_, String color_) {
 		super(apoint_);
-		color = color_;
 	}
 	public String getColor() {
 		/** Visszaadjuk a színt */
-		return color;
+		System.out.println("Szín lekérdezése.");
+		return "Valami";
 	}
 }
--- ModellVasut/src/vasut/Sin.java
+++ ModellVasut/src/vasut/Sin.java
@@ -1,38 +1,40 @@
 package vasut;
 
 public class Sin {
-	private Sin aPoint;
-	private Sin bPoint;
-	private VonatElem actVonatElem;
-	private boolean dir;
-	
+
 	public Sin(Sin aPoint_){
 		/** Megkapja az egyik már létrehozott oldalát, többit nullázza. */
-		aPoint = aPoint_;
-		bPoint = null;
-		actVonatElem = null;
+
 	}
 	public Sin getAPoint() {
-		return aPoint;
 		/** Lekérdezi az aPontot */
+		System.out.println("Lekérdezzük az A pontot.");
+		return aPoint;
 	}
 	public Sin getBPoint() {
-		return aPoint;
 		/** Lekérdezi a bPontot */
+		System.out.println("Lekérdezzük a B pontot.");
+		return new Sin(this);
 	}
 	public void setAPoint(Sin ap) {
 		/** Beállítja az aPontot */
+		System.out.println("Beállítjuk az A pontot.");
 	}
 	public void setBPoint(Sin bp) {
 		/** Beállítja a bPontot */
+		System.out.println("Beállítjuk a B pontot.");
 	}
 	public void setActVonatElem(VonatElem akt) {
 		/** Beállítjuk a actVonatElem-et */
+		System.out.println("Beállítjuk a Sin-en lévõ Mozdonyt.");
 	}
 	public VonatElem getActVonatElem() {
 		/** Lekérdezzük az actVonatElem-et */
+		System.out.println("Lekérdezzük a Sin-en lévõ Mozdonyt.");
+		return actVonatElem;
 	}
 	public Sin actMove() {
 		/** Továbbrakjuk az actVonatElem-et */
+		System.out.println("Meghívódott az actMove() függvény.");
 	}
 }
