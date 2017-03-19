1bfa858f4ddba10a61ebb70c70f4852c09669916
Beni <tarap.beni@gmail.com>
2017-03-19 21:30:29 +0100

    plutty ott vannak újra :) immár 2.1

--- ModellVasut/src/vasut/Jatek.java
+++ ModellVasut/src/vasut/Jatek.java
@@ -3,27 +3,14 @@ package vasut;
 import java.util.*;
 
 public class Jatek {
-	private Palya actGame;	//a Játék tartalmazhat egy pályát
-	private int aktMap;		//aktuális pálya
-	private String sugo;	//a Sugó szövege
 	
 	public void newGame(){
-		Palya seged = null;
-		//itt majd betölti az elsõ pályát...
-		actGame = seged;
-		actGame.run();
+		System.out.println("Jatek.newGame()");
 	}
 	public void nextMap(){
-		Palya seged = null;
-		//itt majd betölti az következõ pályát...
-		actGame = seged;
-		actGame.run();
+		System.out.println("Jatek.nextMap()");
 	}
 	public void mapLoad(){
-		Palya seged = null;
-		//itt majd betölt egy pálya elmentett állását...
-		actGame = seged;
-		actGame.run(); 
+		System.out.println("Jatek.mapLoad()");
 	}
-
 }
--- ModellVasut/src/vasut/Kezdopont.java
+++ ModellVasut/src/vasut/Kezdopont.java
@@ -5,38 +5,36 @@ import java.util.*;
 import vasut.*;
 
 public class Kezdopont extends Sin{
-	//minden idõ [s]-ben értendõ
-	private int time;				//0-tól a spawnTime-ig megy majd egy vonat lerakásakor újra nulla lesz
-	private int spawnTime;			//két vonat indulása között eltelt idõ
-	private int startTime;			//a játék indulása és az elsõ vonat indítása közötti idõ
-	private int trains;				//a még indításra váró vonatok darabszáma (ha 0-nál kisebb, akkor végtelen)
-	private List<Sin> spawnTunnel;	//ide rakja le a KezdoPont a vonatot (ezek a sínek láthatatlanok)
-	private List<VonatElem> train;	//ebben a listában lesz az éppen lerakás alatt lévõ vonat.
-
 	
-	public Kezdopont(Sin aPoint_, int spt, int stt, int tr) {
+	public Kezdopont(Sin aPoint_) {		
 		super(aPoint_);
+		System.out.println("Kezdopont létrejött");
 		//beállítodnak az attribútumok
 		//elkészük a "spawnTunnel"
 	}
 	
-	public void SpawnTrain(){
+	public void spawnTrain(){
+		System.out.println("Kezdopont.spawnTrain()");
 		System.out.println("Hány kocsija legyen a vonatnak?");
+		Bekeres b = new Bekeres();
+		int i = 0;
 		try {
-			int i = System.in.read();
-			//a válasz alapján létrehoz egy vonatot, min 2, vagy 3 kell h legyen majd		
+			i = (int)b.valaszbekeres().toCharArray()[0];
 		} catch (IOException e) {
 			System.out.print(e.getMessage());
 		}
+		System.out.println("Létrejött a vonat "+i+" darab kocsival");
 	}
 	
 	public VonatElem work(){
+		System.out.println("Kezdopont.work()");
 		System.out.println("Jöjjön új vonat?	i/n");
 		try {
-			char c = (char)System.in.read();
+			Bekeres b = new Bekeres();
+			char c = b.valaszbekeres().toCharArray()[0];
 			if(c=='i'){ // itt még ezer dolgot meg fog vizsgálni h mikor jöjjön vonat de majd csak a kész programban ;)
-				SpawnTrain();
-				return train.get(0);
+				spawnTrain();
+				return null;
 			}					
 		}
 		catch (IOException e) {
