87bcb6377223cabda96c3608632cf39f3bc2f89d
Gyula <gyula-26@hotmail.com>
2017-03-19 22:53:07 +0100

    printek címkézve

--- ModellVasut/src/vasut/Mozdony.java
+++ ModellVasut/src/vasut/Mozdony.java
@@ -6,7 +6,7 @@ public class Mozdony extends VonatElem {
 	
 	public boolean run() throws Exception {
 		/** Elindul a mozdony és maga után húzza a kocsijait. */
-		System.out.println("Megindul a vonat. Állomáson keresztül megy? (I/N): ");
+		System.out.println("Mozdony: Megindul a vonat. Állomáson keresztül megy? (I/N): ");
 		if(new Bekeres().valaszbekeres().equals("I"))
 			new Allomas(null, "0").actMove();
 		else 
@@ -14,19 +14,19 @@ public class Mozdony extends VonatElem {
 		
 		System.out.println("Van előttem sín? (I/N): ");
 		if(new Bekeres().valaszbekeres().equals("I")) {
-			System.out.println("Továbbhaladunk, húzom a kocsim.");
+			System.out.println("Mozdony: Továbbhaladunk, húzom a kocsim.");
 			new Sin(null).setActVonatElem(null);
 			new Sin(null).setActVonatElem(this);
 			new Kocsi().pull();
 			new Kocsi().getColor();
-			System.out.println("Szürke az utolsó kocsi? (I/N): ");
+			System.out.println("Mozdony: Szürke az utolsó kocsi? (I/N): ");
 			if(new Bekeres().valaszbekeres().equals("I"))
 				return true;
 			else
 				return false;
 		}
 		else
-			System.out.println("Kisiklottunk.");
+			System.out.println("Mozdony: Kisiklottunk.");
 		return false;
 		
  			
--- ModellVasut/src/vasut/VonatElem.java
+++ ModellVasut/src/vasut/VonatElem.java
@@ -9,14 +9,14 @@ public class VonatElem {
 	public void changeColor() {
 		/** Beállítja a kocsi színét szürkére, az előtte álló kocsi színétől függően. */
 		
-		System.out.println("Meghívódott a színváltás függvényem. Megkérdezem az előző kocsim színét");
+		System.out.println("VonatElem: Meghívódott a színváltás függvényem. Megkérdezem az előző kocsim színét");
 		new VonatElem().getColor();
-		System.out.println("Szürke ez a szín?");
+		System.out.println("VonatElem: Szürke ez a szín?");
 		try {
 			if(new Bekeres().valaszbekeres().equals("I")) 
-				System.out.println("Megváltozott a színem szürkére.");
+				System.out.println("VonatElem: Megváltozott a színem szürkére.");
 			else
-				System.out.println("Nem változott semmi");
+				System.out.println("VonatElem: Nem változott semmi");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -24,28 +24,28 @@ public class VonatElem {
 
 	public Sin getWhereAmI() {
 		/** Lekérdezzük az aktuális tartózkodási helyét */
-		System.out.println("Lekérdezzük, melyik sínen vagyunk.");
+		System.out.println("VonatElem: Lekérdezzük, melyik sínen vagyunk.");
 		return null;
 	}
 	
 	public void setWhereAmI(Sin elem) {
 		/** Beállítja az aktuális tartózkodási helyét */
-		System.out.println("Beállítottuk az aktuális sínünket.");
+		System.out.println("VonatElem: Beállítottuk az aktuális sínünket.");
 	}
 	
 	public void setFrontElem(VonatElem frontElem) {
 		/** Beállítja az előtte álló VonatElem-re mutató referenciát */
-		System.out.println("Beállítottuk az előttem álló vonatelemet.");
+		System.out.println("VonatElem: Beállítottuk az előttem álló vonatelemet.");
 	}
 	
 	public void setBackElem(VonatElem backElem) {
 		/** Beállítja az mögötte álló VonatElem-re mutató referenciát */
-		System.out.println("Beállítottuk a mögöttem álló vonatelemet.");
+		System.out.println("VonatElem: Beállítottuk a mögöttem álló vonatelemet.");
 	}
 	
 	public String getColor() {
 		/** Visszaadja az adott Kocsi színét. */
-		System.out.println("Lekérdezzük a vonatelem színét");
+		System.out.println("VonatElem: Lekérdezzük a vonatelem színét");
 		return null;
 	}
 }
