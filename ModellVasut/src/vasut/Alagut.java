07f95a59eff08c329a48c90bf247097f5a5a7774
AkosRuszka <akosruszka@gmail.com>
2017-03-20 01:33:44 +0100

    Alagut osztály javítás.

--- ModellVasut/src/vasut/Alagut.java
+++ ModellVasut/src/vasut/Alagut.java
@@ -2,23 +2,20 @@ package vasut;
 import java.util.ArrayList;
 
 public class Alagut {
-	public ArrayList<Sin> tunnel = new ArrayList<Sin>(); //Az alagút sínei
-	private KulonlegesHely exitA; //Egyik ki/bejárata
-	private KulonlegesHely exitB; //Másik ki/bejárata
 	
 	Alagut(){}
 	
 	public Sin build(KulonlegesHely hely){
 		//felépíti a kapcsolatot a különleges hellyel
 		System.out.println("Az alagút build() metódusa lefutott");
-		return new Sin();
+		return null;
 		
 	}
 	
 	public Sin destroy(KulonlegesHely hely){
 		//lebontja a kapcsoltatot a különleges hellyel
 		System.out.println("Az alagút destroy() metódusa lefutott");
-		return new Sin ();
+		return null;
 	}
 	
 	public void setExitA(KulonlegesHely a){
