397955f81d380f9e1dc96e3a2dfd5b77d10599a9
AkosRuszka <akosruszka@gmail.com>
2017-03-20 01:11:49 +0100

    kulonlegeshely 2.0

--- ModellVasut/src/vasut/KulonlegesHely.java
+++ ModellVasut/src/vasut/KulonlegesHely.java
@@ -7,18 +7,18 @@ public class KulonlegesHely extends Sin{
 	}
 	
 	public void checkTunnels() throws IOException{ //Megnézi, hogy van-e alagút felépítve már és ez alapján cselekszik
-		System.out.println("chechTunnel() metódus");
-		System.out.println("Tartozik már hozzá alagút?");
+		System.out.println("Különlegeshely: chechTunnel() metódus");
+		System.out.println("Különlegeshely: tartozik már hozzá alagút? (I/N): ");
 		if(new Bekeres().valaszbekeres().equals("I")){
 			new Alagut().destroy(null);
 			setBPoint(null);
-		}
-		
-		System.out.println("Fel van építve máshol alagút ? (I/N):");
-		if(new Bekeres().valaszbekeres().equals("N")){
-			new Alagut().destroy(null);
-			new Alagut().setExitA(null);
-			setBPoint(null);
+		} else {
+			System.out.println("Fel van építve máshol alagút ? (I/N):");
+			if(new Bekeres().valaszbekeres().equals("N")){
+				new Alagut().build(null);
+				new Alagut().setExitA(null);
+				setBPoint(null);
+			}
 		}
 	}
 }
