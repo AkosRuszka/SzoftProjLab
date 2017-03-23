61b167b6e6586d5d487f43efc9ea814587d7ff67
AkosRuszka <akosruszka@gmail.com>
2017-03-23 20:15:06 +0100

    Allomas: metódusok protosítva, szkeleton rész kiszedve

--- ModellVasut/src/vasut/Allomas.java
+++ ModellVasut/src/vasut/Allomas.java
@@ -1,49 +1,32 @@
 package vasut;
 
-import java.io.IOException;
-
 public class Allomas extends Sin{
+	private String color;
 	
-	public Allomas() {
-		/** Jelenleg üres */
+	public Allomas(Sin aPoint_ , String color_){
+		super(aPoint_);
+		color = color_;
 	}
+	
+	
+	/* Csak sintax error kiküszöbölésére, majd ki kell törölni! */
+	public Allomas() {
+	}
+	
+	
 	public String getColor() {
 		/** Visszaadjuk a színt */
 		System.out.println("Állomás színének lekérdezése.");
-		return null;
+		return color;
 	}
 	@Override
 	public Sin actMove() throws Exception {
-		/** nem ugyan az fog történni mint a sima sin actMove()-ban */
 		System.out.println("Állomás: actMove() függvény meghívva.");
 		
-		new VonatElem().getColor(); /* Aktuális rajta lévő vagon (ami meghívta rá a függvényt) */
-		
-		System.out.println("Állomas: Egyezik e a kocsi színe az állomáséval ? (I/N): ");
-		try {
-			if(new Bekeres().valaszbekeres().equals("I")) {
-				new VonatElem().changeColor();
-			} else {
-				System.out.println("Állomas: Nem történik színváltás.");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		/* Vizsgáljuk a rajta áthaladó kocsi színét */
+		if(actVonatElem.getColor().equals(color)) {
+			actVonatElem.changeColor();
 		}
-		
-		new Sin().getActVonatElem();
-		new Sin().getActVonatElem();
-		
-		System.out.println("Állomás: Ütközni fog a vonat ? (I/N): ");
-		try {
-			if(new Bekeres().valaszbekeres().equals("I")) {
-				System.out.println("Állomás: A vonat ütközik.");
-				throw new Exception("Játék vége ütközés miatt.");
-			} else {
-				System.out.println("Állomás: visszaadjuk az egyik Sin referenciát");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return super.actMove();
 	}
 }
