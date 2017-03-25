fa5ea686706a1831cd5dfc06b6c01d4d6759d538
Szilvia <Szilvia@Szilvi-laptop>
2017-03-26 00:21:25 +0100

    Alagut, KulonlegesHely, Palya valtoztatása
    Felesleges kommentek eltüntetése

--- ModellVasut/src/vasut/Alagut.java
+++ ModellVasut/src/vasut/Alagut.java
@@ -2,9 +2,9 @@ package vasut;
 import java.util.ArrayList;
 
 public class Alagut {
-	private ArrayList<Sin> tunnel;
-	private KulonlegesHely exitA;
-	private KulonlegesHely exitB;	
+	private ArrayList<Sin> tunnel;  // Sin rendszer
+	private KulonlegesHely exitA;	//Egyik kivezetés
+	private KulonlegesHely exitB;	//Másik kivezetés
 		
 	Alagut(){
 		//Alagut rendszer felépítése, ami fixen 10 elemű sínrendszerből áll
@@ -20,7 +20,7 @@ public class Alagut {
 	}
 	
 	public Sin build(KulonlegesHely hely){
-		//felépíti a kapcsolatot a különleges hellyel, arra ponjára ami még üres
+		//Felépíti a kapcsolatot a különleges hellyel, arra ponjára ami még üres
 		if(exitA == null){
 			this.setExitA(hely);
 			tunnel.get(0).setAPoint(exitA);
@@ -33,21 +33,10 @@ public class Alagut {
 		}
 		else
 			return null;
-		
-		//Alagut sínrendszerének bekötözése, ha az eleje üres akkor az elejére, ha a vége akkor a végére
-		//Első alkalommal biztosan az elejéhez fűzi
-		/*if(tunnel.get(0).getAPoint() == null){
-			tunnel.get(0).setAPoint(hely);
-			return tunnel.get(0);
-		}
-		else{
-			tunnel.get(tunnel.size()-1).setBPoint(hely);
-			return tunnel.get(tunnel.size()-1);
-		}*/
 	}
 	
 	public Sin destroy(KulonlegesHely hely){
-		//lebontja a kapcsoltatot a különleges hellyel, arról a pontról ahová kapcsolódott az adott KulonlegesHely
+		//Lebontja a kapcsoltatot a különleges hellyel, arról a pontról ahová kapcsolódott az adott KulonlegesHely
 		//Plusz a különleges hely megfelelő pontát is beállítja, hogy ott null érték legyen
 		if(exitA == hely){
 			if(tunnel.get(0) == hely.getAPoint()){
@@ -68,26 +57,7 @@ public class Alagut {
 			}
 			tunnel.get(tunnel.size()-1).setBPoint(null);
 			setExitB(null);
-		}
-		
-		//Sinrendszer beállítása, ki kell nullázni a különleges hely megfelelő pontját és az alagút megfeleő helyét
-		/*if(tunnel.get(0) == hely.getAPoint()){
-			hely.setAPoint(null);
-			tunnel.get(0).setAPoint(null);
-		}
-		else if(tunnel.get(tunnel.size()-1) == hely.getAPoint()){
-			hely.setAPoint(null);
-			tunnel.get(tunnel.size()-1).setBPoint(null);
-		}
-		else if(tunnel.get(0) == hely.getBPoint()){
-			hely.setBPoint(null);
-			tunnel.get(0).setAPoint(null);
-		}
-		else{
-			hely.setBPoint(null);
-			tunnel.get(tunnel.size()-1).setBPoint(null);
-		}*/
-		
+		}		
 		return null;
 	}
 	
--- ModellVasut/src/vasut/KulonlegesHely.java
+++ ModellVasut/src/vasut/KulonlegesHely.java
@@ -14,7 +14,8 @@ public class KulonlegesHely extends Sin{
 		
 	}
 	
-	public void checkTunnels() throws IOException{ //Megnézi, hogy van-e alagút felépítve már és ez alapján cselekszik
+	public void checkTunnels() throws IOException{ 
+		//Megnézi, hogy van-e alagút felépítve már és ez alapján cselekszik
 		/**Alagutat kell építeni, ha a vagy b pontja null, és az aObserver build metódusát, ennek függvényében meghívni
 		   majd frissíteni a csatlakozási pont értékét */
 		if(getAPoint() == null || getAPoint() == null){
@@ -24,29 +25,12 @@ public class KulonlegesHely extends Sin{
 			else if(bPoint == null){
 				setBPoint(aObserver.build(this));
 			}
-		}
-		
+		}		
 		/** Ha nincs null érték, akkor viszont bontani kell az alagutat. 
-		 *  Azt felét kell lebontani, ahol nem null érték van
+		 *  Azt felét kell lebontani, ahol nem null érték van, ezt viszont az alagut elintézi
 		 * */
 		else{
 			aObserver.destroy(this);
 		}
-
-		/*
-		System.out.println("Különlegeshely: chechTunnel() metódus");
-		System.out.println("Különlegeshely: tartozik már hozzá alagút? (I/N): ");
-		if(new Bekeres().valaszbekeres().equals("I")){
-			new Alagut().destroy(null);
-			setBPoint(null);
-		} 
-		else {
-			System.out.println("Fel van építve máshol alagút ? (I/N):");
-			if(new Bekeres().valaszbekeres().equals("N")){
-				new Alagut().build(null);
-				new Alagut().setExitA(null);
-				setBPoint(null);
-			}
-		}*/
 	}
 }
