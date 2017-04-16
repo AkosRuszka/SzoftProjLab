ebef95233ce7160de0b102fb2b2c7a19149b7c21
AkosRuszka <akosruszka@gmail.com>
2017-04-16 19:36:35 +0200

    Sin osztály kész

--- ModellVasut/src/vasut/Sin.java
+++ ModellVasut/src/vasut/Sin.java
@@ -6,7 +6,8 @@ public class Sin implements Serializable{
 	protected VonatElem actVonatElem;
 	protected Sin aPoint;		/** Sin mögötti elem */
 	protected Sin bPoint;		/** Sin előtti elem */
-	protected boolean dir; 	/** Aktuális menetirány (true esetén bPoint)*/
+	protected Sin crossing;		/** Felette levő sín elem, kereszteződéseknél */
+	protected boolean dir; 		/** Aktuális menetirány (true esetén bPoint)*/
 
 	public Sin(Sin aPoint_){
 		/** Megkapja az előtte lévő sínt, többit nullázza. */
@@ -17,58 +18,62 @@ public class Sin implements Serializable{
 		bPoint = null;
 	}
 	
-	
 	/* Ezt majd ki kell szedni, csak a sintax error miatt van még benne!!! */
 	public Sin() {
 		/* Üres konstruktor */
 	}
 	
-	
+	/** Lekérdezi az aPontot */
 	public Sin getAPoint() {
-		/** Lekérdezi az aPontot */
-		System.out.println("Sin: A pont lekérdezése");
+		//System.out.println("Sin: A pont lekérdezése");
 		return aPoint;
 	}
+	
+	/** Lekérdezi a bPontot */
 	public Sin getBPoint() {
-		/** Lekérdezi a bPontot */
-		System.out.println("Sin: B pont lekérdezése");
+		//System.out.println("Sin: B pont lekérdezése");
 		return bPoint;
 	}
+	
+	/** Beállítja az aPontot */
 	public void setAPoint(Sin ap) {
-		/** Beállítja az aPontot */
-		System.out.println("Sin: A pont beállítása.");
+		//System.out.println("Sin: A pont beállítása.");
 		aPoint = ap;
 	}
+	
+	/** Beállítja a bPontot */
 	public void setBPoint(Sin bp) {
-		/** Beállítja a bPontot */
-		System.out.println("Sin: B pont beállítása.");
+		//System.out.println("Sin: B pont beállítása.");
 		bPoint = bp;
 	}
-	public void setActVonatElem(VonatElem akt) {
-		/** Beállítjuk a actVonatElem-et */
-		System.out.println("Sin: mozdony beállítása.");
+	
+	/** Beállítjuk a actVonatElem-et */
+	public void setActVonatElem(VonatElem akt) {	
+		//System.out.println("Sin: mozdony beállítása.");
 		actVonatElem = akt;
 	}
+	
+	/** Lekérdezzük az actVonatElem-et */
 	public VonatElem getActVonatElem() {
-		/** Lekérdezzük az actVonatElem-et */
-		System.out.println("Sin: mozdony lekérdezése");
+		//System.out.println("Sin: mozdony lekérdezése");
 		return actVonatElem;
 	}
+	
+	/** Visszaadjuk hogy merre mehet a vonat */
 	public Sin actMove() throws Exception {
-		/** Visszaadjuk hogy merre mehet a vonat */
-		System.out.println("Sin: actMove() függvényt meghívták.");
+		//System.out.println("Sin: actMove() függvényt meghívták.");
 		
 		VonatElem ap = aPoint.getActVonatElem();
 		VonatElem bp = bPoint.getActVonatElem();
 		
-		/* Ha semelyik irányba nincs kocsi akkor a dir alapján döntünk */
+		/** Ha semelyik irányba nincs kocsi akkor a dir alapján döntünk */
 		if(ap == null && bp == null) {
 			if(dir) {
 				return bPoint;
 			} else {
 				return aPoint;
 			}
-		} else if(ap == null) { 	/* Megvizsgáljuk merre menjen a vonat normális esetben */
+		} else if(ap == null) { 	/** Megvizsgáljuk merre menjen a vonat normális esetben */
 			dir = false;
 			return aPoint;
 		} else if(bp == null) {
@@ -76,7 +81,7 @@ public class Sin implements Serializable{
 			return bPoint;
 		}
 		
-		 /* Ha idáig eljutottunk akkor mindkét irányba van kocsi és ütközés történik! */
+		 /** Ha idáig eljutottunk akkor mindkét irányba van kocsi és ütközés történik! */
 		throw new Exception("Ütközés történt");
 	}
 }
