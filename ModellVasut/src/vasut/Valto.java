07c99425ff2412dd84380343174841f69b292c8d
Gyula <gyula-26@hotmail.com>
2017-04-16 23:51:58 +0200

    Váltó osztály kész, nextState-ben fontos komment!

--- ModellVasut/src/vasut/Valto.java
+++ ModellVasut/src/vasut/Valto.java
@@ -1,20 +1,54 @@
 package vasut;
 
-public class Valto extends Sin{
+import java.util.List;
 
-	public Valto() {
+public class Valto extends Sin{
+	
+	private int actState = 0;
+	private List<Sin> connectPoints;
+
+	public Valto(Sin aPoint_) {
+		super(aPoint_);
 	}
 	
 	public int getActState(){
 		System.out.println("Valto.getActState()");
-		return 0;
+		return actState;
 	}
 	
 	public void nextState(){
 		System.out.println("Valto.nextState())");
+		switch(actState){ // Ide kéne valami okos algoritmus a kapcsolódó sínek átállításához
+		// Jelenlegi módszer: feltesszük, hogy a váltó A pont bPoint-ja, B és C pont aPointja.
+		case 0: // A-ból B-be -> B-ből C-be
+			connectPoints.get(0).setBPoint(null);
+			connectPoints.get(2).setAPoint(this);
+			
+			aPoint = connectPoints.get(1);
+			bPoint = connectPoints.get(2);
+			actState++;
+			break;
+		case 1: // B-ből C-be -> C-ből A-ba
+			connectPoints.get(1).setAPoint(null);
+			connectPoints.get(0).setBPoint(this);
+			
+			aPoint = connectPoints.get(2);
+			bPoint = connectPoints.get(0);
+			actState++;
+			break;
+		case 2: // C-ből A-ba -> A-ból B-be
+			connectPoints.get(2).setAPoint(null);
+			connectPoints.get(1).setAPoint(this);
+			
+			aPoint = connectPoints.get(0);
+			bPoint = connectPoints.get(1);
+			actState = 0;
+			break;
+		}
 	}
 	
 	public void addConnectPoints(Sin a){
 		System.out.println("Valto.addConnectPoints()");
+		connectPoints.add(a);
 	}
 }
