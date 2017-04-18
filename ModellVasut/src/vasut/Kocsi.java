de6033655976abdc70cd94cceb0e84039401a27e
Akos Ruszkabányai <akosruszka@gmail.com>
2017-04-18 11:33:27 +0200

    kocsi.run() függvénye módosítva

--- ModellVasut/src/vasut/Kocsi.java
+++ ModellVasut/src/vasut/Kocsi.java
@@ -19,8 +19,14 @@ public class Kocsi extends VonatElem{
 		
 		/** A mozdony hívja meg az osztálynak ezt a függvényét ha a kocsinak át kell mennie a következő sínre. 
 		 * A sínek segítségével pedig arrébb tudd mozogni a megfelelő irányba. **/
+		
+		Sin idefogokmozogni = whereAmI.actMove();
+		
 		whereAmI.setActVonatElem(null);
-		whereAmI = whereAmI.actMove();
+		
+		whereAmI = idefogokmozogni;
+		idefogokmozogni = null;
+		
 		whereAmI.setActVonatElem(this);
 		if(backElem != null)
 			backElem.pull();
