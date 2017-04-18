09fcbca201fb8f7a77eeafe3c33db5a4fc022685
Akos Ruszkabányai <akosruszka@gmail.com>
2017-04-18 11:30:15 +0200

    megint egy update a mozdony.run()-nak

--- ModellVasut/src/vasut/Mozdony.java
+++ ModellVasut/src/vasut/Mozdony.java
@@ -14,15 +14,18 @@ public class Mozdony extends VonatElem {
 			for (Kocsi k = this.backElem; k != null ; k = k.backElem)
 				lastCart = k;
 		}
-		
+		/* elmentjük az aktuálic elem által visszaadott sin referenciát */
 		Sin idefogokmozogni = whereAmI.actMove();
-		//whereAmI = whereAmI.actMove();
 		
 		if (idefogokmozogni != null){
 			// már megvan hogy merre mozoghatunk ezért nullázzuk az actuális által tárolt VonatElem-et (hisz lejövünk róla)
 			whereAmI.setActVonatElem(null); 
 			
+			/* beállítjuk magunknak az következő sin referenciát*/
 			whereAmI = idefogokmozogni;
+			/* ne mutasson rá több referencia (nem tudom kell e) */
+			idefogokmozogni = null;
+			
 			whereAmI.setActVonatElem(this);
 			
 			backElem.pull();
