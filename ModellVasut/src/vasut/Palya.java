b589bbfb508a97d3664ac4dfc8f9122aa1fe0132
Beni <tarap.beni@gmail.com>
2017-04-18 10:49:36 +0200

    szálas páyla

--- ModellVasut/src/vasut/Palya.java
+++ ModellVasut/src/vasut/Palya.java
@@ -6,18 +6,22 @@ import java.io.Serializable;
 import java.rmi.server.Operation;
 import java.util.ArrayList;
 import java.util.Random;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
-public class Palya implements Serializable{
+public class Palya implements Serializable, Runnable{
+	private static final Logger log = LogManager.getLogger(Palya.class);
+	
 	private boolean speed;
 	private boolean done;
 	private ArrayList<Sin> map;
 	private Kezdopont startPoint;
 	private ArrayList<Mozdony> engines;
 	private Alagut tunnel;
+	private boolean killed = false;
 	
 	public Palya(){
 		map=new ArrayList<Sin>();
-		//startPoint = new Kezdopont(5,6,2);
 		engines = new ArrayList<Mozdony>();
 		speed = false;
 		done = false;
@@ -26,32 +30,74 @@ public class Palya implements Serializable{
 	
 	/** Buttonhoz eseményéhez kötött megszakítás. */
 	public void quitToMain() throws Exception {
-		throw new RuntimeException("quitToMain");
+		synchronized (this){
+			killed=true;
+			Thread.sleep(200);
+			log.info("Exception dobva: quitToMain");
+			throw new RuntimeException("quitToMain");
+		}
 	}
 	
 	/** A játék futása a megfelelő vizsgálatokkal. */
-	public void run() throws Exception{		
-		while(!done) {
-			while(speed) {
+	public void run(){
+		while(true){ 
+			if(speed){
+				//System.out.println("Game thread is doing stuff....."); 
 				Mozdony uj = startPoint.work();
-				if(uj != null) {
-					engines.add(uj); 
+				try {
+					if(uj != null) {
+						engines.add(uj); 
+					}
+					for (Mozdony mozdony : engines) {					
+						done &= mozdony.run();
+					}
+				} catch (Exception e) {						
+					e.printStackTrace();
+					log.info("Hiba elkapva: "+e.getMessage());
+					break;
 				}
-				for (Mozdony mozdony : engines) {
-					done &= mozdony.run();
+				
+				try { 
+					Thread.sleep(200); 
+					} 
+				catch (InterruptedException e) {
+					log.info("Nem várt futási hiba elkapva: "+e.getMessage());
+					e.printStackTrace(); 
+					}
+				if(killed||done){
+					//System.out.println("Game thread is now stopped");
+					break;
 				}
 			}
-		}
-		
+			else{
+				synchronized (this){
+					while(speed){
+						//System.out.println("Game thread is paused");
+						try {
+							wait();
+						} catch (InterruptedException e) {					
+							e.printStackTrace();
+							log.info("Nem várt várakozási hiba elkapva: "+e.getMessage());
+						}
+						if(killed||done){
+							//System.out.println("Game thread is now stopped");
+							break;
+						}
+					}
+				}
+			}
+		}			
 	}
 	
 	/** Megállítjuk illetve elindítjuk a játékot. */
 	public void setStartStop() throws Exception{
-		if(speed) {
-			speed = false;
-		} else {
-			speed = true;
-			//run(); lehet ez nem is kell...
+		synchronized (this){
+			if(speed) {
+				speed = false;
+			} else {
+				speed = true;
+				this.notifyAll();
+			}
 		}
 	}
 
