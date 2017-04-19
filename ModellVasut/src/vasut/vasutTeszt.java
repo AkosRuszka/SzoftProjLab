4220712792364b9272858a473147a5eeae2f1050
Anna Tóth <toth.annelise@gmail.com>
2017-04-20 01:42:38 +0200

    no idea what am i doing

---
+++ ModellVasut/src/vasut/vasutTeszt.java
@@ -0,0 +1,157 @@
+package vasut;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+import vasut.*;
+import org.junit.Test;
+
+public class vasutTeszt {
+	Proto p = new Proto();
+	@Test
+	public void test1() {
+		System.out.println("1. teszt------------------");
+		p.loadInput(1);
+		p.loadOutput(1);
+		p.teszt1();		
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test2() {
+		System.out.println("2. teszt------------------");
+		p.loadInput(2);
+		p.loadOutput(2);
+		p.teszt2();		
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test3() {
+		System.out.println("3. teszt------------------");
+		p.loadInput(3);
+		p.loadOutput(3);
+		p.teszt3();		
+		Assert.assertTrue(p.logCheck());
+	}	
+	@Test
+	public void test4() {
+		System.out.println("4. teszt------------------");
+		p.loadInput(4);
+		p.loadOutput(4);
+		try {
+			p.teszt456789();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		Assert.assertTrue(p.logCheck());
+	}	
+	@Test
+	public void test5() {
+		System.out.println("5. teszt------------------");
+		p.loadInput(5);
+		p.loadOutput(5);
+		try {
+			p.teszt456789();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test6() {
+		System.out.println("6. teszt------------------");
+		p.loadInput(6);
+		p.loadOutput(6);
+		try {
+			p.teszt456789();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test7() {
+		System.out.println("7. teszt------------------");
+		p.loadInput(7);
+		p.loadOutput(7);
+		try {
+			p.teszt456789();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test8() {
+		System.out.println("8. teszt------------------");
+		p.loadInput(8);
+		p.loadOutput(8);
+		try {
+			p.teszt456789();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test9() {
+		System.out.println("9. teszt------------------");
+		p.loadInput(9);
+		p.loadOutput(9);
+		try {
+			p.teszt456789();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		Assert.assertTrue(p.logCheck());
+	} 
+	@Test
+	public void test10() {
+		System.out.println("10. teszt------------------");
+		p.loadInput(10);
+		p.loadOutput(10);
+		p.teszt10111213();			
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test11() {
+		System.out.println("11. teszt------------------");
+		p.loadInput(11);
+		p.loadOutput(11);
+		p.teszt10111213();			
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test12() {
+		System.out.println("12. teszt------------------");
+		p.loadInput(12);
+		p.loadOutput(12);
+		p.teszt10111213();			
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test13() {
+		System.out.println("13. teszt------------------");
+		p.loadInput(13);
+		p.loadOutput(13);
+		p.teszt10111213();			
+		Assert.assertTrue(p.logCheck());
+	}
+	@Test
+	public void test14() {
+		System.out.println("14. teszt------------------");
+		p.loadInput(14);
+		p.loadOutput(14);
+		p.teszt14();			
+		Assert.assertTrue(p.logCheck());
+	}
+
+	@Test
+	public void test15() {
+		System.out.println("15. teszt------------------");
+		p.loadInput(15);
+		p.loadOutput(15);
+		p.teszt15();			
+		Assert.assertTrue(p.logCheck());
+	}
+}
