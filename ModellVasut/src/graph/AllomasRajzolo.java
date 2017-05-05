4efeb22d7acf5ad3b3140841141e9b34d3ec8056
Ákos <kos@Sterben>
2017-05-06 01:52:12 +0200

    AllomasRajzolo és Valtorajzolo elkezdve + graph package

---
+++ ModellVasut/src/graph/AllomasRajzolo.java
@@ -0,0 +1,40 @@
+package graph;
+
+import vasut.Allomas;
+import vasut.IRajzolo;
+
+import java.awt.Image;
+import java.awt.Point;
+
+public class AllomasRajzolo implements IRajzolo{
+	private Allomas allomas;
+	private Image img;
+	private Point coord;
+	public boolean visible;
+	
+	public AllomasRajzolo(Image img, Point coord, Allomas allomas) {
+		this.img = img;
+		this.coord = coord;
+		this.allomas = allomas;
+	}
+
+	public Point getCoord() {
+		return coord;
+	}
+
+	public void setCoord(Point coord) {
+		this.coord = coord;
+	}
+
+	@Override
+	public void rajzol() {
+		/* Kérdés mi kell ide... */
+		
+	}
+
+	@Override
+	public Object getObject() {
+		return this;
+	}
+
+}
---
+++ ModellVasut/src/graph/ValtoRajzolo.java
@@ -0,0 +1,41 @@
+package graph;
+
+import vasut.Valto;
+import vasut.IRajzolo;
+
+import java.awt.Image;
+import java.awt.Point;
+
+public class ValtoRajzolo implements IRajzolo{
+	private Valto valto;
+	private Image img;
+	private Point coord;
+	public boolean visible;
+	
+	public ValtoRajzolo(Image img, Point coord, Valto valto) {
+		/* Koordináta és kép beállítása */
+		this.img = img;
+		this.coord = coord;
+		this.valto = valto;
+	}
+	
+	public Point getCoord() {
+		return coord;
+	}
+
+	public void setCoord(Point coord) {
+		this.coord = coord;
+	}
+
+	@Override
+	public void rajzol() {
+		/* Kérdés mi kell ide... */
+		
+	}
+
+	@Override
+	public Object getObject() {
+		return this;
+	}
+
+}
