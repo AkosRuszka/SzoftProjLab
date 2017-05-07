3353218f1a74eb24005d70eda72004d2a9141df2
Szilvia <Szilvia@Szilvi-laptop>
2017-05-07 22:03:07 +0200

    Kulonlegeshely és mozdony rajzoló hozzáadva

---
+++ ModellVasut/src/graph/KulonlegesHelyRajzolo.java
@@ -0,0 +1,54 @@
+package graph;
+
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import vasut.KulonlegesHely;
+
+public class KulonlegesHelyRajzolo extends JButton implements IRajzolo{
+	private KulonlegesHely kh;
+	private Image img;
+	private Point coord;
+	
+	public KulonlegesHelyRajzolo(Image img, Point coord, KulonlegesHely kh) {
+		
+		setLocation(coord);
+		setIcon(new ImageIcon(img));
+		
+		addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+				  kh.checkTunnels();
+			  }
+		});
+		
+		this.kh = kh;
+		this.img = img;
+		this.coord = coord;
+		
+	}
+
+	
+	public Point getCoord() {
+		return coord;
+	}
+
+	public void setCoord(Point coord) {
+		this.coord = coord;
+	}
+
+	
+	
+	@Override
+	public void rajzol() {
+		// TODO Hajaj... :D
+		
+	}
+
+	@Override
+	public Object getObject() {
+		return kh;
+	}
+}
---
+++ ModellVasut/src/graph/MozdonyRajzolo.java
@@ -0,0 +1,39 @@
+package graph;
+
+import java.awt.Image;
+import java.awt.Point;
+
+import vasut.Mozdony;
+
+public class MozdonyRajzolo implements IRajzolo{
+	private Mozdony mozdony;
+	private Image img;
+	private Point coord;
+	public boolean visible;
+	
+	public MozdonyRajzolo(Image img, Point coord, Mozdony m) {
+		this.img = img;
+		this.coord = coord;
+		this.mozdony = m;
+		visible = true;
+	}
+
+	public Point getCoord() {
+		return coord;
+	}
+
+	public void setCoord(Point coord) {
+		this.coord = coord;
+	}
+
+	@Override
+	public void rajzol() {
+		/* TODO: logika */
+		
+	}
+
+	@Override
+	public Object getObject() {
+		return mozdony;
+	}
+}
