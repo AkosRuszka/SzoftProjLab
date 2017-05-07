b7695f82153d2079d938d585f20807e7b0e997e7
Anna Tóth <toth.annelise@gmail.com>
2017-05-07 02:20:12 +0200

    kocsirajzolo done

---
+++ ModellVasut/src/graph/KocsiRajzolo.java
@@ -0,0 +1,34 @@
+package graph;
+
+import java.awt.Image;
+import java.awt.Point;
+
+import vasut.Allomas;
+
+public class KocsiRajzolo implements IRajzolo {
+	protected Image img;		// A Kocsit ábrázoló képfájl.
+	private Point coord;		// A grafikus objektum x, y koordinátái.
+	private boolean visible;	// A megjelenésért felelős.
+	
+	public KocsiRajzolo(Image img_, Point coord_) {
+		img = img_;
+		coord = coord_;
+	}
+	
+	public void rajzol()		//Az objektum újrarajzolása a grafikus felületen.
+	{
+		//??
+	}
+	public Point getCoord()		// A koordináták lekérdezése.
+	{
+		return coord;
+	}
+	public void setCoord(Point coord_)	// A koordináták beállítása.
+	{
+		coord = coord_;
+	}
+	public Object getObject()	// Az általa ismert objektum visszaadása vizsgálatra.
+	{
+		return this;
+	}
+}
