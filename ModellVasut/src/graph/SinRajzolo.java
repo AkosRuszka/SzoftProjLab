534d423faf9802089d78e8b9e814e7d38b0a3e16
Gyula <gyula-26@hotmail.com>
2017-05-07 19:45:58 +0200

    SinRajzoló logika nélkül

---
+++ ModellVasut/src/graph/SinRajzolo.java
@@ -0,0 +1,40 @@
+package graph;
+
+import java.awt.Image;
+import java.awt.Point;
+
+import vasut.Allomas;
+import vasut.Sin;
+
+public class SinRajzolo implements IRajzolo{
+	private Sin sin;
+	private Image img;
+	private Point coord;
+	public boolean visible;
+	
+	public SinRajzolo(Image img, Point coord, Allomas sin) {
+		this.img = img;
+		this.coord = coord;
+		this.sin = sin;
+	}
+
+	public Point getCoord() {
+		return coord;
+	}
+
+	public void setCoord(Point coord) {
+		this.coord = coord;
+	}
+
+	@Override
+	public void rajzol() {
+		/* TODO: logika */
+		
+	}
+
+	@Override
+	public Object getObject() {
+		return sin;
+	}
+
+}
