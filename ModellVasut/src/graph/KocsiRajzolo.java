d987dee2d7ff5d8d55fb313dcf013c57079eec11
HSzilvia <HSzilvia@152.66.179.233>
2017-05-09 01:33:48 +0200

    Kocsi/Mozdony rajzolo -> nincs kész, to many kérdés :(

--- ModellVasut/src/graph/KocsiRajzolo.java
+++ ModellVasut/src/graph/KocsiRajzolo.java
@@ -2,18 +2,24 @@ package graph;
 
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import vasut.Kocsi;
 
-public class KocsiRajzolo implements IRajzolo{
+public class KocsiRajzolo implements IRajzolo, ActionListener{
 	private Kocsi kocsi;
-	private Image img;
+	private Image print_img;
+	private Image empty_img;
+	private Image notempty_img;
 	private Point coord;
 	public boolean visible;
 	
-	public KocsiRajzolo(Image img, Point coord, Kocsi kocsi) {
+	public KocsiRajzolo(Image print_img, Image empty_img, Image notempty_img, Point coord, Kocsi kocsi) {
 		this.kocsi = kocsi;
-		this.img = img;
+		this.print_img = notempty_img;
+		this.notempty_img = notempty_img;
+		this.empty_img = empty_img;
 		this.coord = coord;
 		visible = true;
 	}
@@ -46,5 +52,17 @@ public class KocsiRajzolo implements IRajzolo{
 	public void setPoint(Point point) {
 		coord = point;
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equals("KOCSI_MOVED")){
+			//?
+		}
+		else if(e.getActionCommand().equals("KOCSI_NOTEMPTY")){
+			print_img = notempty_img;
+		}
+		else //IDE KÉNE EGY EVENT, CSAK NINCS A KOCSIBA MÁSHONNAN KELL HA KIÜRÜLT
+			print_img = empty_img;		
 	}
 }
