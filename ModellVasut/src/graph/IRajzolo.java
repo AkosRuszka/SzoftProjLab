f32b2c1dad49cf3be06a89dda327e0173204d52b
Ákos <kos@Sterben>
2017-05-08 19:59:58 +0200

    Irajzolo -> setPoint/getPoint

--- ModellVasut/src/graph/IRajzolo.java
+++ ModellVasut/src/graph/IRajzolo.java
@@ -1,6 +1,10 @@
 package graph;
 
+import java.awt.Point;
+
 public interface IRajzolo {
 	public void rajzol();
 	public Object getObject();
+	public Point getPoint();
+	public void setPoint(Point point);
 }
