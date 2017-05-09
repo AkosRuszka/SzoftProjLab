4832b04bbfdb954d20092b1db0197f5f1313d7c5
Beni <tarap.beni@gmail.com>
2017-05-09 15:48:44 +0200

    okés a kocsi / mozdony rajzolás :)

--- ModellVasut/src/graph/MozdonyRajzolo.java
+++ ModellVasut/src/graph/MozdonyRajzolo.java
@@ -1,21 +1,40 @@
 package graph;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import vasut.Mozdony;
 
-public class MozdonyRajzolo implements IRajzolo, ActionListener{
+public class MozdonyRajzolo extends JLabel implements IRajzolo, ActionListener{
 	private Mozdony mozdony;
-	private Image print_img;
+	private ImageIcon print_img;
 	private Point coord;
+	private Insets insets;
 	
-	public MozdonyRajzolo(Image img, Point coord, Mozdony m) {
-		this.print_img = img;
+	public MozdonyRajzolo(Point coord, Mozdony m, Insets frameinsets) {
+		this.print_img = new ImageIcon("\\img\\mozdony.png");
 		this.coord = coord;
 		this.mozdony = m;
+		this.insets = frameinsets;
+		
+		try{
+			setIcon(print_img);
+		}
+		catch (Exception e) {
+			e.getMessage();
+		}
+		
+		Dimension size = getPreferredSize();
+		setBounds((int)coord.getX() + insets.left, (int)coord.getY() + insets.top,
+	             size.width, size.height);
 	}
 
 	@Override
@@ -33,6 +52,11 @@ public class MozdonyRajzolo implements IRajzolo, ActionListener{
 		coord = point;
 	}
 
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);	
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		//?
