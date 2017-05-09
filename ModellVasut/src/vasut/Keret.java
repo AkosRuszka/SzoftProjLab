efd65723bfd65dde3c7becc8bc4b81054cdc0965
Beni <tarap.beni@gmail.com>
2017-05-09 13:10:52 +0200

    1 menü mindfölött...

--- ModellVasut/src/vasut/Keret.java
+++ ModellVasut/src/vasut/Keret.java
@@ -7,7 +7,7 @@ public class Keret {
 public static void main(String[] args) throws Exception {
 		Jatek j = new Jatek();
 		View v = new View();
-		Menu m = new Menu();
+		Menu m = new Menu(j);
 		Vezerlo vez = new Vezerlo(v,m,j);
 		//event----------------------------------
 		RailEvent re = new RailEvent(j, 2, vez);
