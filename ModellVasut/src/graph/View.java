2228571184889bf50d0bc6d70f8626cf8c026772
Ákos <kos@Sterben>
2017-05-09 23:56:22 +0200

    View - Javítva

--- ModellVasut/src/graph/View.java
+++ ModellVasut/src/graph/View.java
@@ -77,25 +77,40 @@ public class View extends JFrame{
 		
 		Insets insets = this.getInsets();
 		
+		SinRajzolo ujsin;
+		AllomasRajzolo ujallomas;
+		KulonlegesHelyRajzolo ujkulhely;
+		ValtoRajzolo ujvalto;
+		
 		for(int x=0; x<30; x++) {
 			for(int y=0; y<30; y++) {
-				switch((mymapp[x][y]).getType()) {
-				case "Sin":
-					railElements.add(new SinRajzolo(new Point((x+1)*20, (y+1)*20),mymapp[x][y],insets));
-					break;
-				case "Allomas":
-					/* Hozzáadjuk a framehez már itt a nyilat, amit az állomásoknál szeretnénk megjeleníteni
-					 * mivel az állomásnak meg kell kapnia azt a nyil referenciát ami már rajta van a frame-n */
-					JLabel nyil = new JLabel();
-					add(nyil);
-					railElements.add(new AllomasRajzolo(new Point((x+1)*20, (y+1)*20),(Allomas)mymapp[x][y],insets, nyil));
-					break;
-				case "KulonlegesHely":
-					railElements.add(new KulonlegesHelyRajzolo(new Point((x+1)*20, (y+1)*20),(KulonlegesHely)mymapp[x][y],insets));
-					break;
-				case "Valto":
-					railElements.add(new ValtoRajzolo(new Point((x+1)*20,(x+1)*20),(Valto)mymapp[x][y],insets));
-					break;
+				if(mymapp[x][y] != null){
+					switch((mymapp[x][y]).getType()) {
+					case "Sin":
+						ujsin = new SinRajzolo(new Point((x+1)*20, (y+1)*20),mymapp[x][y],insets);
+						railElements.add(ujsin);
+						add(ujsin);
+						break;
+					case "Allomas":
+						/* Hozzáadjuk a framehez már itt a nyilat, amit az állomásoknál szeretnénk megjeleníteni
+						 * mivel az állomásnak meg kell kapnia azt a nyil referenciát ami már rajta van a frame-n */
+						JLabel nyil = new JLabel();
+						add(nyil);
+						ujallomas = new AllomasRajzolo(new Point((x+1)*20, (y+1)*20),(Allomas)mymapp[x][y],insets, nyil);
+						railElements.add(ujallomas);
+						add(ujallomas);
+						break;
+					case "KulonlegesHely":
+						ujkulhely = new KulonlegesHelyRajzolo(new Point((x+1)*20, (y+1)*20),(KulonlegesHely)mymapp[x][y],insets);
+						railElements.add(ujkulhely);
+						add(ujkulhely);
+						break;
+					case "Valto":
+						ujvalto = new ValtoRajzolo(new Point((x+1)*20,(x+1)*20),(Valto)mymapp[x][y],insets);
+						railElements.add(ujvalto);
+						add(ujvalto);
+						break;
+					}
 				}
 			}
 		}
